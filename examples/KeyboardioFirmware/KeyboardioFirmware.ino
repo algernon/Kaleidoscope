efcc3b4d5dbd270199b2c5e203ff9f7d31405a49
Gergely Nagy <algernon@madhouse-project.org>
2016-12-01 14:56:08 +0100

    Add an EEPROM upload macro
    This is just an example, without much error checking or the like. Just
    to show how it could be done, roughly.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- examples/KeyboardioFirmware/KeyboardioFirmware.ino
+++ examples/KeyboardioFirmware/KeyboardioFirmware.ino
@@ -7,6 +7,9 @@
 #include "Keyboardio-MouseKeys.h"
 #include "Keyboardio-Macros.h"
 #include "KeyboardioFirmware.h"
+
+#define Key_Prog M(0)
+
 #include "generated/keymaps.h"
 
 #include "LED-Off.h"
@@ -40,6 +43,35 @@ static LEDChaseEffect chaseEffect;
 
 static LEDNumlock numLockEffect (NUMPAD_KEYMAP);
 
+static void handleEEPROMUpdate(void) {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  Serial.setTimeout(10000);
+
+  uint8_t keymaps;
+
+  if (Serial.readbytes(&keymaps, 1) == 0)
+    return;
+
+  for (uint16_t i = 0; i < keymaps * ROWS * COLS; i++) {
+    uint16_t keyCode;
+
+    if (Serial.readbytes(&keyCode, 2) != 2)
+      return;
+
+    EEPROM.put(EEPROM_KEYMAP_START + i, keyCode);
+  }
+
+  Storage.defaultKeymap(0);
+  Storage.useEEPROMKeymap(true);
+  Storage.currentKeymaps(keymaps);
+  primary_keymap = temporary_keymap = 0;
+
+  Serial.println("OK");
+};
+
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
     if (macroIndex == 1 && key_toggled_on(keyState)) {
         Serial.print("Keyboard.IO keyboard driver v0.00");
@@ -49,6 +81,9 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
                      W(100),
                      T(0), T(1),
                      END);
+    }
+    if (macroIndex == 0 && key_toggled_on(keyState)) {
+      handleEEPROMUpdate ();
     }
     return MACRO_NONE;
 }
